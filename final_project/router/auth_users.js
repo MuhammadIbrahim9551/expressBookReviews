b710eb10bae11e37cdad75fd4ad216b0478ed12a
Muhammad Ibrahim <nas.61975@yahoo.com>
2026-06-01 16:09:18 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,17 +5,15 @@ const regd_users = express.Router();
 
 let users = [];
 
-module.exports = {
-    users,
-    authenticated: regd_users,
-    isValid
-};
 const isValid = (username) => {
-    return users.some(u => u.username === username);
+    return users.some(user => user.username === username);
 };
 
 const authenticatedUser = (username, password) => {
-    return users.some(u => u.username === username && u.password === password);
+    return users.some(user =>
+        user.username === username &&
+        user.password === password
+    );
 };
 
 // LOGIN
@@ -29,7 +27,7 @@ regd_users.post("/login", (req, res) => {
 
     if (authenticatedUser(username, password)) {
 
-        let accessToken = jwt.sign(
+        const accessToken = jwt.sign(
             { username },
             "fingerprint_customer",
             { expiresIn: "1h" }
@@ -40,12 +38,46 @@ regd_users.post("/login", (req, res) => {
             username
         };
 
-        return res.json({ token: accessToken });
+        return res.status(200).json({ token: accessToken });
     }
 
     return res.status(401).json({ message: "Invalid credentials" });
 });
 
-module.exports.authenticated = regd_users;
-module.exports.users = users;
-module.exports.isValid = isValid;
+// REVIEW (protected)
+regd_users.put("/auth/review/:isbn", (req, res) => {
+
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+
+    const token = req.session.authorization?.token;
+
+    if (!token) {
+        return res.status(403).json({ message: "Not logged in" });
+    }
+
+    let user;
+    try {
+        user = jwt.verify(token, "fingerprint_customer");
+    } catch (err) {
+        return res.status(403).json({ message: "Invalid token" });
+    }
+
+    if (!require("./booksdb.js")[isbn]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    let books = require("./booksdb.js");
+    books[isbn].reviews[user.username] = review || "";
+
+    return res.status(200).json({
+        message: "Review added/updated successfully",
+        reviews: books[isbn].reviews
+    });
+});
+
+module.exports = {
+    authenticated: regd_users,
+    users,
+    isValid
+};
