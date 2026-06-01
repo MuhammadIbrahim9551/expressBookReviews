ef061c7d62f4bae598e1b733f6d6047abb2538af
Muhammad Ibrahim <nas.61975@yahoo.com>
2026-06-01 15:36:36 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,8 @@
+console.log("🔥 REAL GENERAL.JS FILE EXECUTING");
 const express = require('express');
 let books = require("./booksdb.js");
 let users = require("./auth_users.js").users;
 
-console.log("🔥 GENERAL.JS LOADED FROM:", __dirname);
 
 const public_users = express.Router();
 public_users.post("/register", (req, res) => {
