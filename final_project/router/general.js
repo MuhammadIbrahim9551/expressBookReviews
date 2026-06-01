a110b6c411fbc3483f38156d1f1f420aa48d55d9
Muhammad Ibrahim <nas.61975@yahoo.com>
2026-06-01 12:59:41 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,33 +4,25 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 
 const public_users = express.Router();
-
 public_users.post("/register", (req, res) => {
 
     const username = req.body.username;
     const password = req.body.password;
 
-    if (username && password) {
-
-        if (!isValid(username)) {
-
-            users.push({
-                username: username,
-                password: password
-            });
-
-            return res.status(200).json({
-                message: "User successfully registered. Now you can login"
-            });
-        }
-
-        return res.status(404).json({
-            message: "User already exists!"
-        });
+    if (!username || !password) {
+        return res.status(400).json({ message: "Invalid input" });
     }
 
-    return res.status(404).json({
-        message: "Unable to register user."
+    let userExists = users.find(u => u.username === username);
+
+    if (userExists) {
+        return res.status(409).json({ message: "User already exists" });
+    }
+
+    users.push({ username, password });
+
+    return res.status(200).json({
+        message: "User successfully registered. Now you can login"
     });
 });
 
@@ -45,31 +37,34 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
     const isbn = req.params.isbn;
 
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
     return res.status(200).json(books[isbn]);
 });
-
 // Get books by author
 public_users.get('/author/:author', function (req, res) {
 
-    const author = req.params.author;
+    const author = req.params.author.toLowerCase();
 
-    let filteredBooks = Object.entries(books).filter(
-        ([isbn, book]) => book.author === author
+    let result = Object.values(books).filter(
+        book => book.author.toLowerCase() === author
     );
 
-    return res.status(200).json(filteredBooks);
+    return res.status(200).json(result);
 });
 
 // Get books by title
 public_users.get('/title/:title', function (req, res) {
 
-    const title = req.params.title;
+    const title = req.params.title.toLowerCase();
 
-    let filteredBooks = Object.entries(books).filter(
-        ([isbn, book]) => book.title === title
+    let result = Object.values(books).filter(
+        book => book.title.toLowerCase() === title
     );
 
-    return res.status(200).json(filteredBooks);
+    return res.status(200).json(result);
 });
 
 // Get reviews
@@ -77,9 +72,11 @@ public_users.get('/review/:isbn', function (req, res) {
 
     const isbn = req.params.isbn;
 
-    return res.status(200).json(
-        books[isbn].reviews
-    );
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
