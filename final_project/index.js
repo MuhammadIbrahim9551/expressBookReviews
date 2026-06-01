f6fcbe5274b1fa0f32f105f561f1b583430c1f4f
Muhammad Ibrahim <nas.61975@yahoo.com>
2026-06-01 16:27:12 +0530

    Refactor token retrieval in auth middleware

--- final_project/index.js
+++ final_project/index.js
@@ -24,8 +24,7 @@ app.use("/customer/auth/*", function auth(req, res, next) {
 
     if (req.session.authorization) {
 
-        let token = req.session.authorization.token || req.session.authorization.accessToken;
-
+        let token = req.session.authorization.token;
         jwt.verify(token, "fingerprint_customer", (err, user) => {
             if (!err) {
                 req.user = user;
